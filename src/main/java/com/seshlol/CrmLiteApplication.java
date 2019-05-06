7f0c8122bb70a8de1815fb2d4fa882be525bfea9
Seshlol <seshlol@icloud.com>
2019-05-06 23:33:45 +0500

    just started

---
+++ src/main/java/com/seshlol/CrmLiteApplication.java
@@ -0,0 +1,12 @@
+package com.seshlol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrmLiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrmLiteApplication.class, args);
+    }
+}
