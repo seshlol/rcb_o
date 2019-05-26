356553878bcc51cea491d123e5a903f571d98905
Seshlol <seshlol@icloud.com>
2019-05-26 20:11:29 +0500

    just started

---
+++ src/main/java/com/seshlol/controllers/LoginController.java
@@ -0,0 +1,22 @@
+package com.seshlol.controllers;
+
+import com.seshlol.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public LoginController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+}
